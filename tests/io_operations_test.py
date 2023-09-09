ea110e53991cd5471b0b47cdf78fba5899a6cb97
MaxWeise <maxfencing@web.de>
2023-09-09 12:04:15 +0200

    feat: Added tests to the project

---
+++ tests/io_operations_test.py
@@ -0,0 +1,81 @@
+"""Test operations that write to files and folders.
+
+Created: 09.09.2023
+@author: Max Weise
+"""
+
+import logging
+import os
+import pathlib
+import subprocess
+import unittest
+
+from pystrap import main
+
+
+class TestFileWriter(unittest.TestCase):
+    """Test methods that write to files."""
+
+    def setUp(self):
+        """Create the test environment."""
+        self._logger = logging.getLogger("TestFileWriterLogger")
+        self._test_file = pathlib.Path("test_file.txt")
+
+    def test_create_file(self):
+        """Test the correct creation of a file."""
+        file_path = self._test_file
+
+        actual = main.create_file(file_path, self._logger)
+
+        file_exists = file_path.exists()
+        self.assertTrue(actual)
+        self.assertTrue(file_exists)
+
+    def test_create_file_FileExists(self):
+        """Test correct behaviour when a file exists."""
+        file_path = self._test_file
+        with open(file_path, "w+", encoding="utf-8"):
+            pass
+
+        actual = main.create_file(file_path, self._logger)
+        self.assertTrue(actual)
+
+    @unittest.skip("Implemented later")
+    def test_create_file_PathNotValid(self):
+        """Tests the correct error behaviour when the path is not valid."""
+        pass
+
+    def tearDown(self):
+        """Cleanup the test environment."""
+        if self._test_file.exists():
+            os.remove(self._test_file)
+
+
+class TestDirectoryWriter(unittest.TestCase):
+    """Test methods that create directories."""
+
+    def setUp(self):
+        """Create the test environment."""
+        self._logger = logging.getLogger("TestFileWriterLogger")
+
+    @unittest.skip("Fixed later")
+    def test_create_folder(self):
+        """Test the correct creation of a directory."""
+        folder_path = pathlib.Path("test_dir/")
+
+        actual = main.create_folder(folder_path, self._logger)
+
+        folder_exists = folder_path.exists()
+        self.assertTrue(actual)
+        self.assertTrue(folder_exists)
+
+    @unittest.skip("Fixed later")
+    def test_create_folder_FolderExists(self):
+        """Test correct behaviour when a directory exists."""
+        folder_path = pathlib.Path("test_dir/")
+        subprocess.run(["mkdir", folder_path])
+
+        self.assertTrue(folder_path.exists())     # TODO: Can be removed later
+
+        actual = main.create_folder(folder_path, self._logger)
+        self.assertTrue(actual)
