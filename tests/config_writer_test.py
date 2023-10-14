6658b58e83079a62162ac0f944d8ee18867993f6
MaxWeise <maxfencing@web.de>
2023-10-14 18:25:48 +0200

    chore: Removes unused tests

--- tests/config_writer_test.py
+++ tests/config_writer_test.py
@@ -116,55 +116,6 @@ class PyprojecttomlWriterTest(unittest.TestCase):
         if self._test_file.exists():
             os.remove(self._test_file)
 
-class ConfigWriterTest(unittest.TestCase):
-    """Test all config writers."""
-
-    def setUp(self):
-        """Create the test environment."""
-        self._test_file = pathlib.Path("test_file.toml")
-        self._empty_logger = _EmptyLogger()
-
-    @unittest.skip("No longer needed")
-    def test_get_project_config(self):
-        """Test that the attributes get set correctly."""
-        project_name = "test_project"
-
-        actual = get_project_metadata(project_name)
-
-        self.assertIsNotNone(actual)
-        actual_project_data = actual.get("project", None)
-        self.assertIsNotNone(actual_project_data)
-
-        actual_project_name = actual_project_data.get("name", None)
-        self.assertIsNotNone(actual_project_name)
-        self.assertEqual(actual_project_name, "test_project")
-
-    @unittest.skip("No longer needed")
-    def test_get_project_config_withAuthor(self):
-        """Test the correct setting of the author."""
-        project_name = "test_project"
-        author = Author(
-            name="Max Mustermann",
-            email="max.mustermann@examplemail.com"
-        )
-
-        method_return = get_project_metadata(project_name, author=author)
-        actual = method_return.get("project", None)
-
-        self.assertIsNotNone(actual)
-        self.assertEqual(
-            actual.get("authors", None),
-            [{
-                "name": "Max Mustermann",
-                "email": "max.mustermann@examplemail.com"
-            }]
-        )
-
-    def tearDown(self):
-        """Destroy the test environment."""
-        if self._test_file.exists():
-            os.remove(self._test_file)
-
 
 if __name__ == "__main__":
     unittest.main()
